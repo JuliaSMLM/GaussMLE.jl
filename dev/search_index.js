7180c8b15237a933f098fc47ee0923967376b14d
Documenter.jl <documenter@juliadocs.github.io>
2022-12-26 22:44:50 +0000

    build based on 6356c44

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GaussMLE","category":"page"},{"location":"#GaussMLE","page":"Home","title":"GaussMLE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GaussMLE.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GaussMLE]","category":"page"}]
+}
